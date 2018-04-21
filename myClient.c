3b693dbabb479010fb8374cdac7c2ea49996abf4
Pranaya Adhikari <pranaya.adhikari@bison.howard.edu>
2018-04-21 17:15:10 -0400

    file sent to server and conversion sucessfull

--- myClient.c
+++ myClient.c
@@ -5,13 +5,18 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <math.h>
 
 
 #include "sendlib.c"
 
+#define PAYLOAD_SIZE 20		//size of data payload of the RDT layer
+#define HEADER_SIZE 6	
+#define TIMEOUT 50000		//50 milliseconds
+#define TWAIT 10*TIMEOUT	//Each peer keeps an eye on the receiving  end for TWAIT time units before closing 
+#define MAX_LINE 200                         //For retransmission of missing last ACK
 
-
-
+char *make_packet(char* , char* , int* , char*);
 
 int main(int argc, char *argv[]){
     char     *szAddress;             /*  Holds remote IP address   */
@@ -36,12 +41,18 @@ int main(int argc, char *argv[]){
     lossProbabChar = argv[6];
     randomSeedChar = argv[7];
 
-    int port = atoi(argv[1]);
+    int port = atoi(argv[2]);
     double lossProbab = atof(lossProbabChar);
     int randomSeed = atoi(randomSeedChar);
 
     int sockfd;
     struct sockaddr_in serverAddr;
+    char seq_num;
+    int packet_num;
+    char curr_ack;
+    char *data;
+    int data_size;
+    char packet_identifier;
 
 
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -52,59 +63,148 @@ int main(int argc, char *argv[]){
     memset(&serverAddr, '\0', sizeof(serverAddr));
 
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(szAddress);    
+    //serverAddr.sin_addr.s_addr = htonl(szAddress);    
     serverAddr.sin_port = htons(port);
 
-    // if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-    //     perror("bind failed");
-    //     return 0;
-    // }
+    if (inet_aton(szAddress, &serverAddr.sin_addr) <= 0 ) {
+        perror("Error Setting server IP address");
+    }
 
+//    if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+//          perror("bind failed");
+//        return 0;
+//     }
 
     int fileLen, len;
-    int sent=0;
-    char msg[PAYLOAD];
+    //nt sent=0;
+    char msg[PAYLOAD_SIZE];
     FILE *filePtr;
     filePtr = fopen(filePath, "rb");
     if (filePtr==NULL){
         printf("Error opening file!");
         exit(1);
-        goto END;
     }
-  
+
     //Get the length of file
     fseek(filePtr,0, SEEK_END); 
     fileLen= ftell(filePtr); 
     printf("the length of the entire file is: %d\n",fileLen);
     fseek(filePtr, 0, SEEK_SET);
-
-
-    printf("checking for the segfault.......");
-
-    memset(msg, '\0', PAYLOAD);
-    sprintf(msg, "%d", fileLen);
-    rdt_send(sockfd, msg, sizeof(msg), lossProbab, randomSeed, serverAddr);
-    //sendto(sockfd, msg, sizeof(msg), 0,(const struct sockaddr *)&serverAddr, sizeof(serverAddr));  
-
-    printf("Checking for the timeout.........");
-    memset(msg, '\0', PAYLOAD);
-    while (sent < fileLen) {
-    	if ((fileLen-sent) < PAYLOAD)
-    		len = fread(msg, sizeof(char), fileLen-sent, filePtr);
-    	else
-            len = fread(msg, sizeof(char), PAYLOAD, filePtr);
-            printf("The length of 1st client packet sent is %d\n", len);
-    	rdt_send(sockfd, msg, len, lossProbab, randomSeed, serverAddr);
-    	sent += len;
-    }
     
-END:    
-    // close the file
-    fclose(filePtr);
-    // close the rdt socket
-    rdt_close(sockfd,lossProbab, randomSeed, serverAddr);
-	printf("Client program terminated\n");
-    return 0;
+    int number_of_packets = (int) ceil((1.0*fileLen) / PAYLOAD_SIZE);
+    printf("The toatl number of packets: %d\n", number_of_packets);
+    seq_num = '0' ;  // either '1' or '0'
+    packet_num = -1 ; // increase till the last packet from -1 i.e -1 being header packet
+    
+    char fileBuffer[fileLen];
+    
+    fread(fileBuffer,1,fileLen,filePtr);
+    struct timeval timer;
+    
+    // if (sendto(sockfd, fileBuffer, fileLen, 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+    //     printf("Error sending\n");
+    // }
 
+
+    fd_set read_fds;//read file descriptor set
+    FD_ZERO(&read_fds);
+
+    FD_SET(sockfd, &read_fds);
+
+    while (packet_num < number_of_packets) { //there are n+1 packets first one being the packet with all those header
+        // in case of the last packet vs other packets
+        if (packet_num == number_of_packets-1) { 
+            data_size = fileLen - (packet_num)*PAYLOAD_SIZE;
+            packet_identifier = 'l'; //packet identifer: 'l' -> last packet, 'n' -> normal packet, 'h' -> header packet
+        }
+        else {
+            data_size = PAYLOAD_SIZE;
+            packet_identifier = 'n';
+        }
+        
+        //in case of header packet(1st packet) vs other packets
+        if (packet_num == -1) {
+            int targetFileNameLength = strlen(toName); 
+            data_size = targetFileNameLength+5;
+            data = (char*) malloc(data_size);
+            packet_identifier = 'h';
+            memcpy(data, toFormat, 1);
+            memcpy(data+1, &targetFileNameLength, 4);
+            memcpy(data+5, toName, targetFileNameLength);
+        }
+
+        else {
+            data = (char*) malloc(data_size);
+            memcpy(data, fileBuffer+packet_num*PAYLOAD_SIZE, data_size);
+        }
+        
+        len = data_size+HEADER_SIZE;
+        char *send_pack = make_packet(&packet_identifier, &seq_num, &data_size, data);
+        printf("Sending Packet with sequence number  %c\n", seq_num);
+
+        int test;
+        test = lossy_sendto(lossProbab, randomSeed, sockfd, send_pack, len, (struct sockaddr *)&serverAddr, sizeof(serverAddr)); 
+        //test = sendto(sockfd, send_pack, len, 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr)); 
+        printf("WERRRROOROROROROROORRORORO: %d\n", test);
+        if (test < 0) {
+            perror("Error sending the the packet\n");
+            exit(4);
+        }
+        /* TIMER COMES INTO PLAY */
+
+        timer.tv_sec = 0;
+        timer.tv_usec = TIMEOUT;
+
+        int status;
+        status = select(sockfd+1, &read_fds, NULL, NULL, &timer);
+        if (status == -1){
+            perror("select");
+            exit(4);
+        } else if(status == 0){
+            printf("Timeout occured ! Retrasmitting the packet...\n");
+            FD_SET(sockfd, &read_fds);
+            // dynamically deallocating the data and packet to be sent
+            free(data);
+            free(send_pack);
+            continue;
+        }
+
+        socklen_t addr_size = sizeof(serverAddr);
+         /*  RECIEVING THE ACK */
+         int n = 0;
+         if ((n = recvfrom(sockfd, &curr_ack, 1, 0, (struct sockaddr *)&serverAddr, &addr_size)) < 0) 
+         {
+            perror("error receiving ACK");
+            exit(4);
+         } 
+ 
+         if (curr_ack == seq_num) {
+             printf("ACK%c received \n", curr_ack);
+             if (seq_num == '0'){
+                 seq_num = '1';
+             }else{
+                 seq_num = '0';
+             }
+        packet_num++;
+        }   
+         // dynamically deallocating the data and packet to be sent
+         free(data);
+         free(send_pack);
+         
+     }
+     fclose(filePtr);
+     return 0;
+
+
+}
+
+char *make_packet(char* pkt_id, char* pkt_seq, int* size_data, char* data) //every packet has a 6-byte header
+{
+    char* packet = (char*) malloc(PAYLOAD_SIZE+HEADER_SIZE);
+    memcpy(packet, pkt_seq, 1);
+    memcpy(packet+1, pkt_id, 1);
+    memcpy(packet+2, size_data, 4);
+    memcpy(packet+6, data, *size_data);
+    return packet; 
 }
 
--- myServer.c
+++ myServer.c
@@ -5,9 +5,18 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <math.h>
 
 
 #include "sendlib.c"
+#include "helper.c"
+
+#define PAYLOAD_SIZE 20		//size of data payload of the RDT layer
+#define HEADER_SIZE 6	
+#define TIMEOUT 50000		//50 milliseconds
+#define TWAIT 10*TIMEOUT	//Each peer keeps an eye on the receiving  end for TWAIT time units before closing 
+#define MAX_LINE 200                         //For retransmission of missing last ACK
+
 
 
 int main(int argc, char  *argv[]){
@@ -37,9 +46,6 @@ int main(int argc, char  *argv[]){
     struct sockaddr_in my_addr;
     socklen_t addr_size;
     
-    
-   
-   
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("cannot create socket");
         return 0;
@@ -50,8 +56,8 @@ int main(int argc, char  *argv[]){
     memset(&my_addr, '\0', sizeof(my_addr));
 
     my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     my_addr.sin_port = htons(port);
-    //my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
         perror("bind failed");
@@ -60,78 +66,135 @@ int main(int argc, char  *argv[]){
 
     printf("Socket binded successfully\n");
 
-
-    
-
     addr_size = sizeof(my_addr);
-    
+
+    char packet_identifier = 'n'; //for normal packet packet identifier is 'n'
+    char curr_seq_no = '0';
+    char recv_buffer[PAYLOAD_SIZE+HEADER_SIZE];
+    char targetType;
+    int targetFileNameLength;
+    char *targetFileName;
+    int data_size;
+    int offset;
+    unsigned char* data_recv = (unsigned char*) malloc(10);
+    unsigned char* recv_ptr = data_recv;
 
 
-    // FILE *filePtr;
-    // filePtr = fopen(filePath, "rb");
-    // if (filePtr==NULL){
-    //     printf("Error opening file!");
-    //     exit(1);
+
+    int r;
+    char seq_num;
+
+    // if (recvfrom(sockfd, recv_buffer, PAYLOAD_SIZE+HEADER_SIZE, 0, (struct sockaddr *)&my_addr, &addr_size) > 0) {
+    //     printf("The value is %c\n", recv_buffer[0]);
+
     // }
-    while(1){
-    
-    int fileLen, len;
-    char msg[PAYLOAD];
-    int received = 0;
 
-    /* a very simple handshaking protocol */
-	// wait for client request
-	memset(msg, '\0', PAYLOAD);
-    //dt_recv(int fd, char * msg, int length,struct sockaddr_in serverAddr, socklen_t slen)
-    printf("Starting to recieve maal-----\n");
-    len = rdt_recv(sockfd,msg, PAYLOAD, my_addr, addr_size, lossProbab, randomSeed);
-   
-    printf("recieved----yyaay-----\n");
 
-	fileLen = atoi(msg);
-	printf("Received client request: file size = %d\n", fileLen);
+    while (packet_identifier != 'l'){
+        //printf("-----------------The program went here--------------\n");
+        // if ((r = recvfrom(sockfd, recv_buffer, PAYLOAD_SIZE+HEADER_SIZE, 0, (struct sockaddr *)&my_addr, &addr_size) < 0)){
+        //     printf("The value is %d\n", r);
 
-	// if (!testfile) {
-	// 	printf("Cannot open the target file: ./%s for write\n", filepath);
-		
-	// 	// send the ERROR response
-	// 	memset(msg, '\0', MSG_LEN);
-	// 	sprintf(msg, "ERROR");
-	// 	rdt_send(sockfd, msg, strlen(msg));
-	// 	goto END;
-	// } else {
-	// 	printf("Open file %s for writing successfully\n", filepath);
-	// 	// send the ERROR response
-	// 	memset(msg, '\0', MSG_LEN);
-	// 	sprintf(msg, "OKAY");
-	// 	rdt_send(sockfd, msg, strlen(msg));
-	// }
+        // }
+        if ((r = recvfrom(sockfd, recv_buffer, PAYLOAD_SIZE+HEADER_SIZE, 0, (struct sockaddr *)&my_addr,&addr_size) > 0)) {
+            printf("-----------------The program went here--------------\n");
+             memcpy(&seq_num, recv_buffer, 1);
+             printf("Packet with sequence number %c is received \n", seq_num);
+             if (curr_seq_no == seq_num) {
+                 memcpy(&packet_identifier, recv_buffer+1, 1);
+                 printf("PacketIDentifier %c recieved\n", packet_identifier);
+                 if (packet_identifier == 'h') {
+                    recv_buffer[PAYLOAD_SIZE+HEADER_SIZE-1] = '\0';
+                    printf("Buffer is received %s\n", recv_buffer);
+                    memcpy(&targetType, recv_buffer+6, 1);
+                    memcpy(&targetFileNameLength, recv_buffer+7, 4);
+                    targetFileName = (char*) malloc (targetFileNameLength);
+                    memcpy(targetFileName, recv_buffer+11, targetFileNameLength);
+                    // for (int i=0; i<targetFileNameLength; i++) {
+                    //     printf("%c\n", targetFileName[i]);
+                    // }
+                 } else {
+                     memcpy(&data_size, recv_buffer+2, 4);
+                     //printf("Data size: %d,  i: %d\n", payload_size, i);
+                     data_recv = (unsigned char*) realloc(data_recv, offset+data_size);
+                     recv_ptr = data_recv+offset;
+                     memcpy(recv_ptr, recv_buffer+6, data_size);
+                 }
+                 printf("Val of data size: %d\n", data_size);
+                 offset += data_size;
+                 printf("changing seq no of data\n");
+                 if (curr_seq_no == '0'){
+                     curr_seq_no = '1';
+                 }else{
+                     curr_seq_no = '0';
+                 }
+        }
+        printf("sending ack corrsponding to seq num\n");
+        int m;
+        m = lossy_sendto(lossProbab, randomSeed, sockfd, &seq_num, 1, (struct sockaddr *)&my_addr, sizeof(my_addr));
+        if(m < 0) 
+        {
+            perror("Error sending ACK\n");
+            exit(4);
+        } 
+        printf("ACK %c sent\n", seq_num);
 
-	/* start the file transfer */   
-	printf("Start receiving the file . . .\n");
-    // receive the file contents
-    
-    //while (received < fileLen) {
-    	memset(msg, 0, PAYLOAD);
-    	len = rdt_recv(sockfd, msg,PAYLOAD,my_addr, addr_size, lossProbab, randomSeed);
-    	//fwrite(msg, sizeof(char), len, testfile);
-    	received += len;
-    	printf("Received a message of size %d bytes\n", len);
-   // }
-
-    
-
-    printf("Complete the file transfer.\n");
-    
-END:
-     // close the rdt socket
-     rdt_close(sockfd, lossProbab, randomSeed, my_addr);
-     
-     printf("Server program terminated\n");
-     return 0;
     }
+    
+    printf("Total data size: %d\n", offset);
+
+    }
+   
+    //check the recived contents by printing
+    // for (int i=0; i<offset; i++) {
+       //printf("%c\n", data_recv[i]);
+    
+    ///implement convert
+    
+    int written;
+    written = offset-4;
+    char tempBuffer[written];
+    printf("Fine until memcpy\n");
+    memcpy(tempBuffer,data_recv+4,written);
+    printf("The total length written is%d\n", written);
+    
+    FILE *newFilePtr;
+    newFilePtr = fopen(targetFileName, "wb");
+    if (newFilePtr==NULL){
+        printf("Error opening file!");
+        exit(1);
+    }
+   
+    int pointerPosition;
+    pointerPosition = 0;
+    //printf("Fine until while loop \n");
+    while (pointerPosition < written-1){
+        if (tempBuffer[pointerPosition] == 0) {
+            pointerPosition = readTypeZero(tempBuffer, pointerPosition, newFilePtr, &targetType);
+        }
+        if (tempBuffer[pointerPosition]== 1){
+            pointerPosition = readTypeOne(tempBuffer, pointerPosition, newFilePtr, &targetType);
+        }
+    }
+
+    fclose(newFilePtr);
+
+
+     //}
+
+   
+
+    //  if (close(sockfd) < 0 ){
+    //      perror("Closing connection");
+    //      exit(4);
+    //     }
+
+    return 0;
 }
 
+
+
+    
 
 
     
