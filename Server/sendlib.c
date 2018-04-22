cb53696e04544acb3318307de6589526a76b0d35
Pranaya Adhikari <pranaya.adhikari@bison.howard.edu>
2018-04-21 22:44:57 -0400

    some organization fix

---
+++ Server/sendlib.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+
+/* 
+ * This function must NOT be modified.
+ */
+ssize_t lossy_sendto (float loss_ratio, unsigned int random_seed, int sockfd, const void * buf, size_t len,
+                     const struct sockaddr *dest_addr, socklen_t addrlen)
+{
+  int n;
+  float f;
+  static int init = 0;
+  
+  if (! init) {
+    srand (random_seed);
+    init = 1;
+  }
+  
+  f = ((float) rand ()) / RAND_MAX;
+  
+  /* Simulate segment loss */
+  if (f < loss_ratio) return len;
+  
+  return sendto(sockfd, buf, len, 0, dest_addr, addrlen);  
+}
+
+
